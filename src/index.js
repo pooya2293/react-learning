c25ca06dfcf0a9c89706b1a258c4c46c32f1c7df
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-04-25 14:05:59 +0430

    <br />

--- src/index.js
+++ src/index.js
@@ -32,23 +32,26 @@ import ReactDOM from 'react-dom';
 {/***********************/}
 
 // use class for local state and lifecycle hooks
-class App extends React.Component {
+// class App extends React.Component {
 
- constructor(props) {
- // fires before component is mounted
- super(props); // makes this refer to this component
- this.state = {date: new Date()}; // set state
- }
- render() {
- return (
- <h1>
- It is {this.state.date.toLocaleTimeString()}.
- </h1>
- )
- }
-};
+//  constructor(props) {
+//  // fires before component is mounted
+//  super(props); // makes this refer to this component
+//  this.state = {date: new Date()}; // set state
+//  }
+//  render() {
+//  return (
+//  <h1>
+//  It is {this.state.date.toLocaleTimeString()}.
+//  </h1>
+//  )
+//  }
+// };
 
-ReactDOM.render(
-	<App />,
-	document.getElementById('root')
-);
+// ReactDOM.render(
+// 	<App />,
+// 	document.getElementById('root')
+// );
+
+{/***********************/}
+
