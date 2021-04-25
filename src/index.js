ae96a61170f0c0c4f177e41eabc224c2a535bc07
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-04-25 14:03:20 +0430

    simple ES6 class component

--- src/index.js
+++ src/index.js
@@ -16,15 +16,36 @@ import ReactDOM from 'react-dom';
 //   document.getElementById('root')
 // );
 
-const MyComponent = ()=> {
-	return(
-		<div>Hello from MyComponent</div>
-	);
-}
+{/***********************/}
+
+// const MyComponent = ()=> {
+// 	return(
+// 		<div>Hello from MyComponent</div>
+// 	);
+// }
+
+// ReactDOM.render(
+// <MyComponent />,
+// document.getElementById('root')
+// )
+
+{/***********************/}
+
+// use class for local state and lifecycle hooks
+class App extends React.Component {
+
+ constructor(props) {
+ // fires before component is mounted
+ super(props); // makes this refer to this component
+ }
+ render() {
+ return (
+ <h1>Hello from class ES6 component</h1>
+ )
+ }
+};
 
 ReactDOM.render(
-<MyComponent />,
-document.getElementById('root')
-)
-
-
+	<App />,
+	document.getElementById('root')
+);
