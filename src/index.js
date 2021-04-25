f7a97971ab422f61463b84d89588ebe45b7a95c0
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-04-25 13:40:08 +0430

    get in touch in to DOM with simpel Component

--- src/index.js
+++ src/index.js
@@ -2,18 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-const JSX = (
-  <div className='myDiv'>
-    <h1>Hello World</h1>
-    <br />
-    <p>Lets render this to the DOM</p>
-    <hr />
-  </div>
-);
+// const JSX = (
+//   <div className='myDiv'>
+//     <h1>Hello World</h1>
+//     <br />
+//     <p>Lets render this to the DOM</p>
+//     <hr />
+//   </div>
+// );
+
+// ReactDOM.render(
+//  JSX ,
+//   document.getElementById('root')
+// );
+
+const MyComponent = ()=> {
+	return(
+		<div>Hello from MyComponent</div>
+	);
+}
 
 ReactDOM.render(
- JSX ,
-  document.getElementById('root')
-);
+<MyComponent />,
+document.getElementById('root')
+)
 
 
