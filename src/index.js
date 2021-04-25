684d59c7c82b14cb34412180a7f860c65e38334e
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-04-25 20:51:22 +0430

    use array in props

--- src/index.js
+++ src/index.js
@@ -159,4 +159,32 @@ import ReactDOM from 'react-dom';
 // 	document.querySelector('#root')
 // )
 
-/*********Props***********/
+/*********Array*Props***********/
+
+const List = (props) => {
+	return <p>{props.tasks.join(", ")}</p>
+}
+
+class ToDo extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render(){
+		return(
+			<div>
+				<h1>To Do Lists</h1>
+				<h2>Today</h2>
+				<List tasks={["walk dog","workout"]} />
+				<h2>Tomorrow</h2>
+				<List tasks={["walk dog","workout","take a Rest"]} />
+			</div>
+		)
+	}
+};
+
+ReactDOM.render(
+ 	<ToDo />,
+ 	document.querySelector('#root')
+ )
+
+/*********Array*Props***********/
