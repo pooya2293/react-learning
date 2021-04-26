3a76748d039252e39c1ce8d3f82544ff2e0e706a
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-04-26 18:14:49 +0430

    this setState whit handleClick

--- src/index.js
+++ src/index.js
@@ -407,3 +407,35 @@ import ReactDOM from 'react-dom';
 
 /****Set State with this.setState****/
 
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+	    this.state = {
+	      name: 'Initial State'
+	    }
+    	this.handleClick = this.handleClick
+    	.bind(this);
+  };
+  handleClick() {
+    // Change code below this line
+	this.setState({
+	  name: 'React Rocks!'
+	})
+    // Change code above this line
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click Me</button>
+        <h1>{this.state.name}</h1>
+      </div>
+    );
+  }
+};
+
+ReactDOM.render(
+ 	<MyComponent />,
+ 	document.querySelector('#root')
+ )
+
+/****Set State with this.setState****/
