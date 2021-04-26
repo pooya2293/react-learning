2bc823b30acc0fa5be7e772d1f9e9583c26bd72e
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-04-26 18:13:16 +0430

    <br />

--- src/index.js
+++ src/index.js
@@ -378,31 +378,32 @@ import ReactDOM from 'react-dom';
 
 /****Render State in the User Interface Another Way****/
 
-class MyComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'freeCodeCamp'
-    }
-  }
-  render() {
-    // Change code below this line
-		const name =  this.state.name;
-    // Change code above this line
-    return (
-      <div>
-        { /* Change code below this line */ }
-		<h1>{name}</h1>
-        { /* Change code above this line */ }
-      </div>
-    );
-  }
-};
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: 'freeCodeCamp'
+//     }
+//   }
+//   render() {
+//     // Change code below this line
+// 		const name =  this.state.name;
+//     // Change code above this line
+//     return (
+//       <div>
+//         { /* Change code below this line */ }
+// 		<h1>{name}</h1>
+//         { /* Change code above this line */ }
+//       </div>
+//     );
+//   }
+// };
 
-ReactDOM.render(
- 	<MyComponent />,
- 	document.querySelector('#root')
- )
+// ReactDOM.render(
+//  	<MyComponent />,
+//  	document.querySelector('#root')
+//  )
 
 
-/****Render State in the User Interface Another Way****/
+/****Set State with this.setState****/
+
