173647043639163ade449779d97aeee529f8cce8
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-04-26 20:45:09 +0430

    <br />

--- src/index.js
+++ src/index.js
@@ -448,51 +448,51 @@ import ReactDOM from 'react-dom';
 
 /****Use State to Toggle an Element****/
 
-class MyComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visibility: false
-    };
-    // Change code below this line
-  	this.toggleVisibility = this.
-  	toggleVisibility.bind(this);
-    // Change code above this line
-  }
-  // Change code below this line
-	toggleVisibility(){
-	  	this.setState(state=>{
-		    if(state.visibility === true){
-			    return {visibility:false}
-			}else{
-			    return {visibility:true}
-			}
-		})
-	};
-  // Change code above this line
-  render() {
-    if (this.state.visibility) {
-      return (
-        <div>
-          <button onClick={this.
-          toggleVisibility}>Click Me
-          </button>
-          <h1>Now you see me!</h1>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <button onClick={this.toggleVisibility}>Click Me</button>
-        </div>
-      );
-    }
-  }
-};
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       visibility: false
+//     };
+//     // Change code below this line
+//   	this.toggleVisibility = this.
+//   	toggleVisibility.bind(this);
+//     // Change code above this line
+//   }
+//   // Change code below this line
+// 	toggleVisibility(){
+// 	  	this.setState(state=>{
+// 		    if(state.visibility === true){
+// 			    return {visibility:false}
+// 			}else{
+// 			    return {visibility:true}
+// 			}
+// 		})
+// 	};
+//   // Change code above this line
+//   render() {
+//     if (this.state.visibility) {
+//       return (
+//         <div>
+//           <button onClick={this.
+//           toggleVisibility}>Click Me
+//           </button>
+//           <h1>Now you see me!</h1>
+//         </div>
+//       );
+//     } else {
+//       return (
+//         <div>
+//           <button onClick={this.toggleVisibility}>Click Me</button>
+//         </div>
+//       );
+//     }
+//   }
+// };
 
-ReactDOM.render(
- 	<MyComponent />,
- 	document.querySelector('#root')
- )
+// ReactDOM.render(
+//  	<MyComponent />,
+//  	document.querySelector('#root')
+//  )
 
 /****Use State to Toggle an Element****/
