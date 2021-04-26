3444998751e43493ee092a57f4d3d9912ea0217b
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-04-26 10:33:36 +0430

    Access Props Using this.props

--- src/index.js
+++ src/index.js
@@ -252,4 +252,47 @@ import ReactDOM from 'react-dom';
 //  	document.querySelector('#root')
 //  )
 
-/*******Use*Default*Props*********/
+/****Access Props*Using*this.props****/
+
+class ReturnTempPassword extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+            { /* Change code below this line */ }
+            <p>Your temporary password is: <strong>{this.props.tempPassword}</strong></p>
+            { /* Change code above this line */ }
+        </div>
+    );
+  }
+};
+
+class ResetPassword extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+          <h2>Reset Password</h2>
+          <h3>We've generated a new temporary password for you.</h3>
+          <h3>Please reset this password from your account settings ASAP.</h3>
+         
+			 <ReturnTempPassword 
+			 tempPassword="xxxxxxxx" />
+         
+        </div>
+    );
+  }
+};
+
+ReactDOM.render(
+ 	<ResetPassword />,
+ 	document.querySelector('#root')
+ )
+
+/****Access Props*Using*this.props****/
