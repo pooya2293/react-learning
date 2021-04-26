e4e2de94f21fc85b57978beb3887ee863d3a95a7
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-04-26 11:50:32 +0430

    <br />

--- src/index.js
+++ src/index.js
@@ -353,27 +353,27 @@ import ReactDOM from 'react-dom';
 
  /****Create*a*Stateful*Component****/
 
- class StatefulComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    // Only change code below this line
-  this.state={
-    name:"Name"
-  }
-    // Only change code above this line
-  }
-  render() {
-    return (
-      <div>
-        <h1>{this.state.name}</h1>
-      </div>
-    );
-  }
-};
+//  class StatefulComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     // Only change code below this line
+//   this.state={
+//     name:"Name"
+//   }
+//     // Only change code above this line
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.state.name}</h1>
+//       </div>
+//     );
+//   }
+// };
 
-ReactDOM.render(
- 	<StatefulComponent />,
- 	document.querySelector('#root')
- )
+// ReactDOM.render(
+//  	<StatefulComponent />,
+//  	document.querySelector('#root')
+//  )
 
 /****Create*a*Stateful*Component****/
